03c87497d095271be598200c96fc4aa66fa1c32f
Vladimiriskakov <iskakov.vova@bk.ru>
2024-05-05 14:04:50 +0700

    comit 1

--- kanbancraft_client/src/components/common/kanban/KanbanSidebar.jsx
+++ kanbancraft_client/src/components/common/kanban/KanbanSidebar.jsx
@@ -3,21 +3,67 @@ import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import './kanban.css';
 
+// Define the Subproject component
+const Subproject = ({ subprojectName }) => {
+  return (
+    <div className="subproject" >
+      <div style={{ marginLeft: '15%', fontSize: "25px"}}>{subprojectName}</div>
+      
+    </div>
+  );
+};
+
+// Define the Project component
+const Project = ({ projectName, subprojects }) => {
+  return (
+    <div className="project" style={{ marginLeft: '10%', fontSize: "25px"}}>
+      <div className="projectName">{projectName}</div>
+
+    </div>
+  );
+};
+
 const KanbanSidebar = () => {
+  // Array to store project names
+  const projects = [
+    { projectName: "где", subprojects: ["ну и", "хуиня"] },
+    { projectName: "сковорода", subprojects: ["подпроект3", "подпроект4","ыыыааааа"] },
+    // Add more projects and their subprojects as needed
+  ];
+
   return (
     <div className="kanbanSidebar">
       <div className="userGroup">
         <Avatar className="avatar">U</Avatar>
         <div variant="body1" className="username">Имя пользователя</div>
       </div>
+
       <div className="myProject">
-        <div className="myProjectText">Мои проекты
+        <div className="myProjectText">
+          Мои проекты
           <Button
             variant="contained"
-            color="primary" 
+            color="primary"
             style={{ background: 'transparent', boxShadow: 'none', fontSize: '1.7rem', marginLeft: '100px', minWidth: 'auto'}}
             size="small"
-            >+</Button>
+          >
+            +
+          </Button>
+        </div>
+      </div>
+
+      <div>
+        <div style={{ fontSize: "25px"}}>
+          {projects.map((project, index) => (
+            <div >
+              <div className="projectContainer" key={index}>
+                  <Project projectName={project.projectName} subprojects={project.subprojects} />
+              </div>
+                  {project.subprojects.map((subproject, index) => (
+                  <div className="subprojectContainer" key={index}>
+                  <Subproject subprojectName={subproject} /></div>))}
+              </div>
+          ))}
         </div>
       </div>
     </div>
